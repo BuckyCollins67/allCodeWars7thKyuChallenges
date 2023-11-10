2021e33dda28bcaecac77c789de272d1414d3461
Bucky Collins <bucky.collins.67@gmail.com>
2023-11-09 19:30:35 -0800

    added daily code wars.

---
+++ 11-09-2023-mumbling.js
@@ -0,0 +1,34 @@
+// -------------------------------------------------------------------- //
+// ------------------------CODE WARS DAILY CHALLENGE------------------- //
+
+// This time no story, no theory. The examples below show you how to write function accum:
+
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+//
+// -------------------------------------------------------------------- //
+// ------------------------------PREP METHOD--------------------------- //
+
+// P - Parameters
+// We will be given a string of letters from a to z.
+
+// R - Returns
+// We will be expected to return a string.  The string should consist of the letters of the input string seperated by a hyphen.  The first letter should be capitalized.  The number of characters should be equal to the letters position in the  original string.
+
+// E - Examples
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+// P - Psuedocode
+// 1.) 
+// 2.) 
+// 3.) 
+// 4.) 
+
+// -------------------------------------------------------------------- //
+// --------------------------------SOLUTION---------------------------- //
+
